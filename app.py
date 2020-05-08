c1b4c63897c032b650674f7f0b0f3e916d09e4df
Erfanx <irfansyed479@gmail.com>
2020-05-08 06:25:10 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,28 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("Hello World!")
 
+DATA_URL = (
+
+	"I:\hit-and-run\Motor_Vehicle_Collisions_-_Crashes.csv"
+
+)
+
+st.title("Hit and Run - Motor Vehicle Collisions in New York City")
+st.markdown("### A Streamlit dashboard application to analyze motor vehicle collisions in NY")
+
+@st.cache(persist=True)
+def load_data(nrows):
+	data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[['CRASH_DATE','CRASH_TIME']])
+	data.dropna(subset=['LATITUDE','LONGITUDE'], inplace=True)
+	lowercase = lambda x: str(x).lower()
+	data.rename(lowercase, axis='columns', inplace=True)
+	data.rename(columns={'crash_data_crash_time':'date/time'}, inplace=True)
+	return data
+	
+	
+data = load_data(100000)
+
+st.subheader('Raw Data ')
+st.write(data)
